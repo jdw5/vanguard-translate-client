d9c812c8d85bc8f71bb1e30f4d1d29eb719db58d
jdw5 <joshuadwallace1@gmail.com>
2024-06-21 16:26:07 +1000

    Source and example

---
+++ examples/Example.vue
@@ -0,0 +1,41 @@
+<script setup>
+
+// Dummy props
+const props = {
+    translate: {
+        login: {
+            email: {
+                label: 'Email',
+                placeholder: 'Enter your email',
+            },
+            password: {
+                label: 'Password',
+                placeholder: 'Enter your password',
+            },
+            submit: 'Login',
+        }
+    }
+}
+
+const __ = useTranslate(props.translate)
+</script>
+
+<template>
+    <form>
+        <div>
+            <label for="email">
+                {{ __('login.email.label') }}
+            </label>
+            <input type="email" id="email" :placeholder="__('login.email.placeholder')"  />
+        </div>
+        <div>
+            <label for="password">
+                {{ __('login.password.label') }}
+            </label>
+            <input type="password" id="password" :placeholder="__('login.password.placeholder')"  />
+        </div>
+        <button type="submit"> 
+            {{ __('login.submit') }}
+        </button>
+    </form>
+</template>
---
+++ vite.config.ts
@@ -0,0 +1,15 @@
+import { resolve } from "path"
+import { defineConfig } from "vite"
+
+export default defineConfig({
+  build: {
+    lib: {
+      entry: resolve(__dirname, "src/index.ts"),
+      name: "Vanguard Translate",
+      fileName: `vanguard-translate`,
+    },
+    rollupOptions: {
+      external: ["vue", "@inertiajs/vue3", "axios"],
+    },
+  },
+})
