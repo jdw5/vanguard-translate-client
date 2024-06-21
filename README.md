2f47b2babe72658c743de349922f28b5b6fbd9fd
jdw5 <joshuadwallace1@gmail.com>
2024-06-21 16:32:55 +1000

    Fixed issue with build command

---
+++ README.md
@@ -0,0 +1,1 @@
+# Vanguard Translate Client
--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,24 @@
-import { usePage } from '@inertiajs/vue3'
+import { usePage } from "@inertiajs/vue3";
 
-export type VanguardTranslateProps = object|null
+export type VanguardTranslateProps = object | null;
 
-const page = usePage()
+const page = usePage();
 
-export const unwrap = (obj: object) => {
-    const unwrapped = {}
-    Object.keys(obj).forEach(key => {
-        unwrapped[key] = obj[key].value
-    })
-    return unwrapped
-}
+const unwrap = (obj: object) => {
+	const unwrapped = {};
+	Object.keys(obj).forEach((key) => {
+		unwrapped[key] = obj[key].value;
+	});
+	return unwrapped;
+};
 
 export default (props: VanguardTranslateProps = null) => {
-    // Unwrap the props as dot notation
-    const translations = unwrap(props === null ?
-        {} :
-        page.props.translations)
+	// Unwrap the props as dot notation
+	const translations = unwrap(props === null ? {} : page.props.translations);
 
-    const retrieve = (key: string) => {
-        return translations[key] || key
-    }
+	const retrieve = (key: string) => {
+		return translations[key] || key;
+	};
 
-    return retrieve
-}
+	return retrieve;
+};
